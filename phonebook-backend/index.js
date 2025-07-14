11685a526dfb4a0037d322087741961a0cabe531
jchvz <jchvz.dev@gmail.com>
2025-07-14 12:12:14 -0700

    Completed exercise 3.8

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -26,7 +26,9 @@ let persons = [
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status - :response-time ms - :body'))
   
 app.get('/api/persons', (request, response) => {
     response.json(persons)
