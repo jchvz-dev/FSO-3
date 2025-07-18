e91868912d05be705a5a2cde040621d05b0c247b
jchvz <jchvz.dev@gmail.com>
2025-07-18 13:11:31 -0700

    Completed exercise 3.13

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,28 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-
-let persons = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
+const Contact = require('./models/contact')
 
 const app = express()
 app.use(express.json())
@@ -32,7 +10,9 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status - :response-time ms - :body'))
   
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
 })
 
 app.get('/info', (request, response) => {
